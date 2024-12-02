0a507b97c69fe13e7b36e4708bff1be4ad8c5111
Bjorn Wictorin <bjorn.wictorin@gmail.com>
2024-12-02 20:31:25 +0100

    day2

---
+++ day2/report1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def check_levels(levels):
+    level_diffs = [int(levels[ii+1]) - int(levels[ii]) for ii in range(len(levels) - 1)]
+    return all(4 > diff > 0 for diff in level_diffs) or all(-4 < diff < 0 for diff in level_diffs)
+
+def main():
+    reports = open("input.txt", "r")
+    safe_reports = 0
+    for line in reports:
+        levels = line.split(" ")
+        if check_levels(levels):
+            safe_reports += 1
+
+    print(safe_reports)
+
+
+if __name__ == "__main__":
+    main()
