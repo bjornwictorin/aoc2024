89d896b8f848418a43343a51bc9f94d8b3690d36
Bjorn Wictorin <bjorn.wictorin@gmail.com>
2025-01-23 21:02:45 +0100

    day21 part 2

--- day21/keypads2.py
+++ day21/keypads2.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python3
 
-NUM_DIR_ROBOTS = 2
+from functools import cache
+from typing import List
+
+NUM_DIRPADS = 25 + 1
 
 
 def parse_input(filename):
@@ -123,75 +126,74 @@ def is_valid_dir_robot(pos):
     return True
 
 
-def calc_next_state(init_state, button):
-    """Return state after pressing button. If state is invalid return None"""
-    state = list(init_state)
-    for ii in range(NUM_DIR_ROBOTS):
-        # print(button)
-        y, x = state[ii]
-        if button == "A":
-            # button = state.dir_robots[ii].symbol
-            button = get_dir_pad_symbol(y, x)
-        else:
-            if button == ">":
-                state[ii] = (y, x + 1)
-            elif button == "v":
-                state[ii] = (y + 1, x)
-            elif button == "<":
-                state[ii] = (y, x - 1)
-            elif button == "^":
-                state[ii] = (y - 1, x)
-            else:
-                assert False, f"invalid button: {button}"
-            if is_valid_dir_robot(state[ii]):
-                return tuple(state)
-            else:
-                # print(f"invalid dir robot: {state.dir_robots[ii]}")
-                return None
+def calc_pad_path_len(from_pos, to_pos):
+    from_row, from_col = from_pos
+    to_row, to_col = to_pos
+    return abs(to_row - from_row) + abs(to_col - from_col)
 
-    # update num_robot
-    if button != "A":
-        # to_y, to_x = get_num_pad_pos(to_digit)
-        num_robot_y, num_robot_x = state[-1]
-        if button == ">": # and num_robot_x < to_x:
-            state[-1] = (num_robot_y, num_robot_x + 1)
-        elif button == "v": # and num_robot_y < to_y:
-            state[-1] = (num_robot_y + 1, num_robot_x)
-        elif button == "<": # and num_robot_x > to_x:
-            state[-1] = (num_robot_y, num_robot_x - 1)
-        elif button == "^": # and num_robot_y > to_y:
-            state[-1] = (num_robot_y - 1, num_robot_x)
-        else:
-            return None
-            # assert False, f"invalid button: {button}"
-    if is_valid_num_robot(state[-1]):
-        return tuple(state)
+
+def find_pad_shortest_paths(pos, target_pos, steps, direction, path: List[str], max_steps, numpad_shortest_paths: List[str], is_numpad):
+    # print(f"pos: {pos}")
+    row, col = pos
+    if steps > max_steps:
+        return
+    if is_numpad and not is_valid_num_robot(pos):
+        return
+    if not is_numpad and not is_valid_dir_robot(pos):
+        return
+    if steps > 0:
+        path.append(direction)
+    if pos == target_pos:
+        path.append("A")
+        numpad_shortest_paths.append("".join(path))
+        path.pop()
     else:
-        # print(f"invalid num robot: {state.num_robot}")
-        return None
+        find_pad_shortest_paths((row, col + 1), target_pos, steps + 1, ">", path, max_steps, numpad_shortest_paths, is_numpad)
+        find_pad_shortest_paths((row + 1, col), target_pos, steps + 1, "v", path, max_steps, numpad_shortest_paths, is_numpad)
+        find_pad_shortest_paths((row, col - 1), target_pos, steps + 1, "<", path, max_steps, numpad_shortest_paths, is_numpad)
+        find_pad_shortest_paths((row - 1, col), target_pos, steps + 1, "^", path, max_steps, numpad_shortest_paths, is_numpad)
+    # Remove last entry before returning
+    if steps > 0:
+        path.pop()
+
+def calc_pad_shortest_paths(from_digit, to_digit, is_numpad=True):
+    # print(f"{from_digit} to {to_digit}")
+    # Find length of shortest paths in num keypad
+    if is_numpad:
+        pad_path_len = calc_pad_path_len(get_num_pad_pos(from_digit), get_num_pad_pos(to_digit))
+    else:
+        pad_path_len = calc_pad_path_len(get_dir_pad_pos(from_digit), get_dir_pad_pos(to_digit))
+    # print(f"numpad_path_len: {numpad_path_len}")
+    # Use DFS recursion to find all
+    pad_shortest_paths = []
+    from_pos = get_num_pad_pos(from_digit) if is_numpad else get_dir_pad_pos(from_digit)
+    target_pos = get_num_pad_pos(to_digit) if is_numpad else get_dir_pad_pos(to_digit)
+    path = []
+    find_pad_shortest_paths(from_pos, target_pos, 0, "*", path, pad_path_len, pad_shortest_paths, is_numpad)
+    assert len(pad_shortest_paths) >= 1, f"Error: no paths of length {pad_path_len} found"
+    assert all(len(path) == pad_path_len + 1 for path in pad_shortest_paths) # +1 for pressing A
+    return pad_shortest_paths
 
 
-def calc_transition_len(from_digit, to_digit):
-    """Use BFS to find shortest sequence which leads to pressing to_digit"""
-    from_digit_y, from_digit_x = get_num_pad_pos(from_digit)
-    initial_state = tuple(((0, 2) if ii < NUM_DIR_ROBOTS else (from_digit_y, from_digit_x)) for ii in range(NUM_DIR_ROBOTS + 1))
-    current_states = [initial_state]
-    visited_states = set()
-    num_steps = 0
-    while True:
-        num_steps += 1
-        next_states = []
-        for state in current_states:
-            if all(dir_robot == (0, 2) for dir_robot in state[:-1]) and state[-1] == get_num_pad_pos(to_digit):
-                # print(num_steps)
-                return num_steps
-            # create new next states by pressing all buttons on remote control
-            for button in "^A<v>":
-                next_state = calc_next_state(state, button)
-                if next_state and tuple(next_state) not in visited_states:
-                    next_states.append(next_state)
-                    visited_states.add(tuple(next_state))
-        current_states = next_states
+@cache
+def calc_path_len(from_digit, to_digit, level):
+    # Find all shortest paths in num keypad
+    if level == NUM_DIRPADS:
+        return 1
+    numpad_shortest_paths = calc_pad_shortest_paths(from_digit, to_digit, is_numpad=level == 0)
+    # print(f"level: {level}, numpad_shortest_paths {numpad_shortest_paths}")
+    path_lengths = []
+    for numpad_path in numpad_shortest_paths:
+        path_len = 0
+        for ii in range(len(numpad_path)):
+            from_symbol = "A" if ii == 0 else numpad_path[ii - 1]
+            to_symbol = numpad_path[ii]
+            path_len += calc_path_len(from_symbol, to_symbol, level + 1)
+        path_lengths.append(path_len)
+    
+    # print(len(path_lengths))
+    assert min(path_lengths) > 0
+    return min(path_lengths)
 
 
 def calc_seq_len(code):
@@ -199,21 +201,24 @@ def calc_seq_len(code):
     for ii in range(len(code)):
         from_digit = "A" if ii == 0 else code[ii - 1]
         to_digit = code[ii]
-        seq_len += calc_transition_len(from_digit, to_digit)
-        # print(".", end="", flush=True)
+        partial_len = calc_path_len(from_digit, to_digit, 0)
+        seq_len += partial_len
+        # print(f"from: {from_digit}, to: {to_digit}, partial_len: {partial_len}")
     return seq_len
 
 
 def main():
     codes = parse_input("input.txt")
     values = [int(x[:-1]) for x in codes]
+    # codes = ["1"]
+    # values = [1]
     seq_lengths = []
 
     for code in codes:
         print(code)
         seq_lengths.append(calc_seq_len(code))
     
-    print(f"NUM_DIR_ROBOTS: {NUM_DIR_ROBOTS}, seq_lengths: {seq_lengths}")
+    print(f"NUM_DIRPADS: {NUM_DIRPADS}, seq_lengths: {seq_lengths}")
 
     complexities = [value * length for value, length in zip(values, seq_lengths)]
     print(sum(complexities))
